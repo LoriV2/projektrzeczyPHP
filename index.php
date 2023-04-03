fc1ba8f96941526fb5ee8f31375c341aa5ebd176
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2023-04-03 12:36:17 +0200

    rzeczy

--- index.php
+++ index.php
@@ -23,13 +23,13 @@ if (isset($_GET['Strona'])) {
 	</title>
 </head>
 <header>
-	<nav class="navbar navbar-expand-lg">
+	<nav class="navbar navbar-expand-lg ">
 		<div class="container-fluid">
 			<a class="navbar-brand" href="index.php?Strona=StronaGłówna">O_O</a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon">|||</span>
 			</button>
-			<div class=" justify-content-end collapse navbar-collapse" id="navbarNavDropdown">
+			<div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
 				<ul class="navbar-nav">
 					<li class="nav-item">
 						<a class="nav-link" aria-current="page" href="index.php?Strona=StronaGłówna">Strona główna</a>
